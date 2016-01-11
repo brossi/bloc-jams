fc7af64f735a2b742ddd9fb2e22312b99d336bfd
Ben Rossi <ben@rossinet.com>
2016-01-11 12:11:55 -0500

    Assignment 23: Custom  utility function.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,32 +1,30 @@
-var animatePoints = function animatePoints() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var revealPoint = function revealPoint(point) {
+    point.style.opacity = 1;
+    point.style.transform = 'scaleX(1) translateY(0)';
+    point.style.msTransform = 'scaleX(1) translateY(0)';
+    point.style.WebkitTransform = 'scaleX(1) translateY(0)';  
+};
 
-    var revealFirstPoint = function revealFirstPoint() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = 'scaleX(1) translateY(0)';
-        points[0].style.msTransform = 'scaleX(1) translateY(0)';
-        points[0].style.WebkitTransform = 'scaleX(1) translateY(0)';
-    };
+var animatePoints = function animatePoints(pointsArray) {
 
-    var revealSecondPoint = function revealSecondPoint() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = 'scaleX(1) translateY(0)';
-        points[1].style.msTransform = 'scaleX(1) translateY(0)';
-        points[1].style.WebkitTransform = 'scaleX(1) translateY(0)';
-    };
-
-    var revealThirdPoint = function revealThirdPoint() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = 'scaleX(1) translateY(0)';
-        points[2].style.msTransform = 'scaleX(1) translateY(0)';
-        points[2].style.WebkitTransform = 'scaleX(1) translateY(0)';                   
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-    
+    // iterate through the 'points' elements and apply a transition in sequence
+    forEach(pointsArray, revealPoint); 
 };
             
-            
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
